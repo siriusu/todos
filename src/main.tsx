d04a87e65e029cf1ce7265dbe1e5dc05c18473fb
siriusu <siriusu09@gmail.com>
2025-06-06 23:40:44 +0900

    feat: Implement React Router v7 setup (3-A)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx'; // App.tsx が存在しない場合は作成します
-import './index.css'; // Tailwind CSS をインポート
+import Home from './pages/Home.tsx';
+// import './index.css'; // Tailwind CSS をインポート
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todos" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
 );
